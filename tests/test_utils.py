0597ccc2b2fe4ddc9db61734c9574af1525b8fea
valentinacanol991 <vcano@sorcero.com>
2022-06-15 13:26:01 -0400

    tests: Add test for new method html_table_to_markdown_table
    ENG-2056

---
+++ tests/test_utils.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2020 Sorcero, Inc.
+#
+# This file is part of Sorcero's Language Intelligence platform
+# (see https://www.sorcero.com).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+
+from bs4 import BeautifulSoup
+
+from ingestum import sources
+from ingestum import utils
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def test_html_table_to_markdown_table():
+    source = sources.HTML(path=os.path.join(ROOT_DIR, "tests/input/html_table.html"))
+    with open(source.path) as html:
+        content = html.read()
+
+    soup = BeautifulSoup(content, "xml")
+    table = utils.html_table_to_markdown_table(soup)
+
+    with open(
+        os.path.join(ROOT_DIR, "tests/output/html_table_to_markdown_table.txt")
+    ) as f:
+        output = "".join(f.readlines())
+
+    assert table == output
